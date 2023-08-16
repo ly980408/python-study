2e8de79aeb3555431a07b6052b99afad44524df6
Yangyang <282568728@qq.com>
2023-08-16 11:34:49 +0800

    update: flask demo

--- demos/Flask/app.py
+++ demos/Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import pymysql
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+def connect_db():
+    return pymysql.connect(host="127.0.0.1", port=3306,
+                           user="root", password="123456",
+                           db="test_db", charset="utf8")
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
@@ -26,9 +30,7 @@ def add_user():
         age = int(age)
 
     # insert data into db
-    conn = pymysql.connect(host="127.0.0.1", port=3306,
-                           user="root", password="123456",
-                           db="test_db", charset="utf8")
+    conn = connect_db()
     cursor = conn.cursor()
     sql = "INSERT INTO user_table (name, age) VALUES (%(name)s, %(age)s);"
     cursor.execute(sql, {'name': name, 'age': age})
@@ -41,14 +43,11 @@ def add_user():
 
     return '添加成功'
 
-
-@app.route('/user_list', methods=['GET'])
-def user_list():
-    conn = pymysql.connect(host="127.0.0.1", port=3306,
-                           user="root", password="123456",
-                           db="test_db", charset="utf8")
+@app.route('/get_user_list', methods=['GET'])
+def get_user_list():
+    conn = connect_db()
     cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
-
+    
     cursor.execute("SELECT * FROM user_table;")
 
     user_list = cursor.fetchall()
@@ -56,6 +55,11 @@ def user_list():
     cursor.close()
     conn.close()
 
+    return user_list
+
+@app.route('/user_list', methods=['GET'])
+def user_list():
+    user_list = get_user_list()
     return render_template('user_list.html', user_list=user_list)
 
 
